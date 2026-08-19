cde4a34573a6c22544fa05121a1810908d464bbb
faithit <faithittrainer@gmail.com>
2026-08-19 12:05:10 +0300

    render

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ cd myproject
 python manage.py migrate --noinput
 python manage.py create_deploy_superuser || true
 python manage.py collectstatic --noinput
-exec gunicorn myproject.wsgi --bind 0.0.0.0:${PORT:-8000
+exec gunicorn myproject.wsgi --bind 0.0.0.0:${PORT:-8000}
