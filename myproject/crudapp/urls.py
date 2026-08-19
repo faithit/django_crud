dbb82884020c4ea0c89a2e49e02a2f918d98e5ac
faithit <faithittrainer@gmail.com>
2026-08-19 11:03:23 +0300

    cloudinary

--- myproject/crudapp/urls.py
+++ myproject/crudapp/urls.py
@@ -8,7 +8,11 @@ urlpatterns=[
    path('addproduct/',views.add_product,name='addproduct'),
    path('delete/<int:id>/',views.delete_product,name='delete'),
    path('update/<int:id>/',views.update_product,name='update'),
-   path('register/',views.register_user,name='register')
+   path('register/',views.register_user,name='register'),
+   path('login/',views.login_user,name='login'),
+   path('user/',views.user_dashboard,name='user'),
+   path('logout/',views.logout_user,name='logout')
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myproject/crudapp/views.py
+++ myproject/crudapp/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import render,redirect
 from .forms import ProductForm,RegisterForm
 from .models import Product
 from django.shortcuts import get_object_or_404
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login,logout
+from django.contrib.auth.decorators import login_required,user_passes_test
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
-#R-read-fetch data from db and display in admin dashboard   
+    #fetch only 3
+    products =Product.objects.all()[:3]
+    carousel_items=Product.objects.filter(image__isnull=False).exclude(image__exact='')[:3]
+    return render(request, 'index.html',{'products':products,'carousel_items':carousel_items})
+#R-read-fetch data from db and display in admin dashboard 
+@login_required(login_url='login')
+@user_passes_test(lambda u:u.is_staff,login_url='login')
 def  admin(request):
     products=Product.objects.all()
     return render(request, 'admin_dashboard.html',{'products': products})
 #create-add data to db using forms
+@user_passes_test(lambda u:u.is_staff,login_url='login')
+@login_required(login_url='login')
 def add_product(request):
     if request.method == 'POST':
         form=ProductForm(request.POST,request.FILES)
@@ -21,11 +30,15 @@ def add_product(request):
         form=ProductForm()
     return render(request, 'addproduct.html',{'form': form})
 #D-delete data from db
+@user_passes_test(lambda u:u.is_staff,login_url='login')
+@login_required(login_url='login')
 def delete_product(request,id):
     product=get_object_or_404(Product,id=id)
     product.delete()
     return redirect('products')
 #u-update existing data in db
+@user_passes_test(lambda u:u.is_staff,login_url='login')
+@login_required(login_url='login')
 def update_product(request,id):
     product=get_object_or_404(Product,id=id)
     if request.method == 'POST':
@@ -46,5 +59,28 @@ def register_user(request):
     else:
         form=RegisterForm()
     return render(request,'register.html',{'form':form})
+#login a user
+def login_user(request):
+    if request.method=="POST":
+        form=AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+             user=form.get_user()
+             login(request,user)
+             if user.is_staff:
+                return redirect('products')
+             else:
+                 return redirect('user')
+    else:
+        form=AuthenticationForm()
+    return render(request,'login.html',{'form':form})
+@login_required(login_url='login')
+def  user_dashboard(request):
+    products=Product.objects.all()
+    return render(request, 'users/user_dashboard.html',{'products':products})
+#logout user
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
     
 
